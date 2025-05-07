0e09b6c342e1eb84f9f45315b090595ec665e3fb
Thanh Nguyen <vayne112233@gmail.com>
2025-05-08 01:54:46 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,33 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use((req, res, next) => {
-    let data = '';
-    req.setEncoding('utf8');
-    req.on('data', (chunk) => {
-      data += chunk;
-    });
-    req.on('end', () => {
-      req.rawBody = data;
-      console.log('Raw body:', data);
-      next();
-    });
-  });
+  // app.use((req, res, next) => {
+  //   let data = '';
+  //   req.setEncoding('utf8');
+  //   req.on('data', (chunk) => {
+  //     data += chunk;
+  //   });
+  //   req.on('end', () => {
+  //     req.rawBody = data;
+  //     console.log('Raw body:', data);
+  //     next();
+  //   });
+  // });
 
-  // Cấu hình body-parser
-  app.use(
-    bodyParser.json({
-      verify: (req: any, res, buf) => {
-        req.rawBody = buf.toString();
-      },
-    }),
-  );
+  // // Cấu hình body-parser
+  // app.use(
+  //   bodyParser.json({
+  //     verify: (req: any, res, buf) => {
+  //       req.rawBody = buf.toString();
+  //     },
+  //   }),
+  // );
 
   console.log('1::>>', 1);
 
-  // app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(bodyParser.text());
-  // app.use(bodyParser.raw({ type: '*/*' }));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.text());
+  app.use(bodyParser.raw({ type: '*/*' }));
 
   await app.listen(3000);
   console.log('2::>>', 2);
