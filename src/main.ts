be266bc0e250582a29e5e93ee7c5ca974bd495c7
Thanh Nguyen <vayne112233@gmail.com>
2025-05-08 00:58:32 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
     bodyParser: false,
   });
-  app.useBodyParser('text');
+
+  app.use((req, res, next) => {
+    let data = '';
+    req.setEncoding('utf8');
+    req.on('data', (chunk) => {
+      data += chunk;
+    });
+    req.on('end', () => {
+      req.rawBody = data;
+      console.log('Raw body:', data);
+      next();
+    });
+  });
+
+  // Cấu hình body-parser
+  app.use(
+    bodyParser.json({
+      verify: (req, res, buf) => {
+        req.rawBody = buf.toString();
+      },
+    }),
+  );
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.text());
+  app.use(bodyParser.raw({ type: '*/*' }));
   await app.listen(3000);
 }
 bootstrap();
