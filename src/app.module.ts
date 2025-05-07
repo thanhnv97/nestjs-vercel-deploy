53a86329bf7813f689284c25823353cfbc15fef9
Thanh Nguyen <vayne112233@gmail.com>
2025-05-07 23:01:20 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RawBodyMiddleware } from './decorators/parse-raw-body.decorator';
