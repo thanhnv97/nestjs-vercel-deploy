cd631d9c76f57e10493a57f378e1136288c3641a
Thanh Nguyen <vayne112233@gmail.com>
2025-05-08 02:20:03 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,10 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomBodyParserMiddleware } from './middleware/custome-body.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(CustomBodyParserMiddleware)
-      .forRoutes({ path: '*', method: RequestMethod.ALL }); //Alternative way to apply to all routes
-  }
-}
+export class AppModule {}
