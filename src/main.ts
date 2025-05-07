2fe6c93db3d43d6bd137d1b19c364a0db7ada608
Thanh Nguyen <vayne112233@gmail.com>
2025-05-07 23:38:02 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.text());
+  app.use(bodyParser.raw({ type: '*/*' }));
+
   await app.listen(3000);
 }
 bootstrap();
