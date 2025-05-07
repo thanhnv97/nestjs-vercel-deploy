524ae7b1d8f70be73acb94de7608c89794665ba6
Thanh Nguyen <vayne112233@gmail.com>
2025-05-07 23:09:10 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,10 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RawBodyMiddleware } from './decorators/parse-raw-body.decorator';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(RawBodyMiddleware)
-      .forRoutes({ path: 'webhook', method: RequestMethod.POST });
-  }
-}
+export class AppModule {}
