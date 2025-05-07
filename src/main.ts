22897af47445fc3899ad7a981fa217e8f529b00e
Thanh Nguyen <vayne112233@gmail.com>
2025-05-08 00:04:02 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
   });
+  app.useBodyParser('text');
   await app.listen(3000);
 }
 bootstrap();
