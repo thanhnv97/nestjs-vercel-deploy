2934719d7653ee5218e14e997fe4247d661e4d3a
Thanh Nguyen <vayne112233@gmail.com>
2025-05-09 03:10:31 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bodyParser: false,
   });
-  //
-  // app.use((req, res, next) => {
-  //   let data = '';
-  //   req.setEncoding('utf8');
-  //   req.on('data', (chunk) => {
-  //     data += chunk;
-  //   });
-  //   req.on('end', () => {
-  //     req.rawBody = data;
-  //     req.body = data;
-  //     next();
-  //   });
-  // });
 
-  // // Cấu hình body-parser
-  // app.use(
-  //   bodyParser.json({
-  //     verify: (req: any, res, buf) => {
-  //       req.rawBody = buf.toString();
-  //     },
-  //   }),
-  // );
+  const options = new DocumentBuilder()
+    .setTitle('API docs')
+    .setVersion('1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
-  console.log('1::>>', 1);
-
-  // app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(bodyParser.text());
-  // app.use(bodyParser.raw({ type: '*/*' }));
-
-  // app.useBodyParser('json');
-  // app.useBodyParser('text');
-  // app.useBodyParser('raw');
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
 
   await app.listen(3000);
-  console.log('2::>>', 2);
 }
 bootstrap();
